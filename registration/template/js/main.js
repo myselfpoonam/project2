5e371085bde6ed253f66bca86ae168b52be55f3b
myselfpoonam <118976088+myselfpoonam@users.noreply.github.com>
2023-05-20 19:59:16 +0545

    Add files via upload

---
+++ registration/template/js/main.js
@@ -0,0 +1,34 @@
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+var swiper = new swiper(".popular-content", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextE1: ".swiper-button-next",
+        prevE1: ".swiper-button-prev",
+
+    },
+});
